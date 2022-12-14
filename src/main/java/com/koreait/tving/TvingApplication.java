4c92a4a7dad4ad9f88728b017e2ca2b59e00bcef
e88rice <guswns8780@naver.com>
2022-12-14 16:21:03 +0900

    create project

---
+++ src/main/java/com/koreait/tving/TvingApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.tving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvingApplication.class, args);
+    }
+
+}
